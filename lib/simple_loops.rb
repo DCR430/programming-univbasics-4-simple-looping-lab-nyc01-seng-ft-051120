83176d65c166b4bf45e02121e5151adfeb72cf95
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-24 21:36:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,9 +6,10 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message,integer)
+def loop_message_n_times(message,n)
   count = 0
-  while count < ("integer")
+  n=0
+  while count < ("n")
     puts message
     count += 1
   end
